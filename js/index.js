d31ce856b6ab1fd19b3b71e4bb5d2932e71a2dcb
Vitor M Cometti <comettivitor@gmail.com>
2022-12-20 22:22:14 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 const pokemonContainer = document.querySelector('.container');
-const quantPokemon = 151;
+const container = document.querySelector('.container');
 
 const fetchPokemons = async () => {
-    for (let i = 1; i <= quantPokemon; i++) {
+    for (let i = 1; i <= 151; i++) {
+        await getPokemon(i);
+    }
+}
+
+const fetchPokemons2 = async () => {
+    for (let i = 152; i <= 251; i++) {
         await getPokemon(i);
     }
 }
@@ -14,23 +20,31 @@ const getPokemon = async id => {
     createCard(pokemon);
 }
 
-fetchPokemons();
+const atualizaPagina = () =>{
+    if(container.parentNode){
+        container.parentNode.removeChild(container)
+    }
+}
 
 function createCard(pokemon) {
     const pokemonElement = document.createElement("div");
     pokemonElement.classList.add('pokemon');
 
-    const id = pokemon.id
+    const id = pokemon.id;
     const nome = pokemon.name.toUpperCase();
+    const tipo = pokemon.types
+        .map((type) => type.type.name)
+        .join(' ');
 
     const pokeInnerHTML = `
     <div class="pokemon-container">
         <div class="imagem-pokemon">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"/>
+            <img src="https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg"/>
         </div>
         <div class="info">
         <h2 class="id" >#${id}</h2>
         <h1 class="name" >${nome}</h1>
+        <h3 class="type">${tipo}</h3>
         </div>
     </div>
     `;
